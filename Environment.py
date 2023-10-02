06fa3465436781953dd48279d5381c176800991f
Igor <ipovaric@gmail.com>
2023-10-01 23:08:13 -0600

    feat: set default and update table values

--- Environment.py
+++ Environment.py
@@ -1,22 +1,32 @@
 import numpy as np
 
 class Environment:
-    def __init__(self, board):
-        self.board
 
-    def render_board():
+    def __init__(self):
         # Create a 9x9 matrix with all elements as 0
-        board = [[1 for _ in range(9)] for _ in range(9)]
+        self.board = np.zeros((9, 9), dtype=int)
+    
+    def set(self,value,row,col):
+        # update the value at row, col location
+        self.board[row-1][col-1] = value
 
+    def render_board(self):
+
+        print()
         # Print the Sudoku board
         for i in range(9):
             for j in range(9):
-                print(board[i][j], end=' ')
+                print(self.board[i][j], end=' ')
                 if (j + 1) % 3 == 0 and j < 8:
                     print("|", end=' ')
             print()
             if (i + 1) % 3 == 0 and i < 8:
                 print("-" * 21)
+        print()
 
-e = Environment
+e = Environment()
+e.set(5,1,1)
+e.set(9,4,6)
+e.set(7,9,1)
+e.set(3,5,5)
 e.render_board()
